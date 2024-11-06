87a1805d699a9186bcce4dbb53d7e2d1789b248c
MohanRaja <155057473+MohanRaja-tech@users.noreply.github.com>
2024-11-06 18:31:04 +0530

    Merge k sorted Lists.py
    Leetcode problem No :  23

---
+++ Merge k sorted Lists.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        lst = []
+        for i in lists:
+            temp=i
+            while(temp!=None):
+                lst.append(temp.val)
+                temp=temp.next
+        dummy = ListNode(0)
+        current = dummy
+        for i in sorted(lst):
+            current.next = ListNode(i)
+            current = current.next
+        return dummy.next
+        
